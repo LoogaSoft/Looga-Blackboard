16697b64406161755eac3b9efe659eae1a32f329
Ben Holicek <bh4786@gmail.com>
2026-06-19 09:47:49 -0500

    static shortcuts

--- Runtime/LoogaBlackboardRegistry.cs
+++ Runtime/LoogaBlackboardRegistry.cs
@@ -18,5 +18,86 @@ namespace LoogaSoft.Blackboard
                 Active = null;
             }
         }
+
+        public static void SetValue(LoogaBlackboardKey key, LoogaBlackboardValue value)
+        {
+            ActiveWriter?.SetValue(key, value);
+        }
+
+        public static void SetBool(LoogaBlackboardKey key, bool value)
+        {
+            ActiveWriter?.SetBool(key, value);
+        }
+
+        public static void SetInt(LoogaBlackboardKey key, int value)
+        {
+            ActiveWriter?.SetInt(key, value);
+        }
+
+        public static void SetFloat(LoogaBlackboardKey key, float value)
+        {
+            ActiveWriter?.SetFloat(key, value);
+        }
+
+        public static void SetString(LoogaBlackboardKey key, string value)
+        {
+            ActiveWriter?.SetString(key, value);
+        }
+
+        public static bool TryGetValue(LoogaBlackboardKey key, out LoogaBlackboardValue value)
+        {
+            if (ActiveReader != null)
+            {
+                return ActiveReader.TryGetValue(key, out value);
+            }
+
+            value = default;
+            return false;
+        }
+
+        public static bool TryGetBool(LoogaBlackboardKey key, out bool value)
+        {
+            return ActiveReader.TryGetBool(key, out value);
+        }
+
+        public static bool GetBool(LoogaBlackboardKey key)
+        {
+            return ActiveReader.GetBool(key);
+        }
+
+        public static bool TryGetInt(LoogaBlackboardKey key, out int value)
+        {
+            return ActiveReader.TryGetInt(key, out value);
+        }
+
+        public static int GetInt(LoogaBlackboardKey key)
+        {
+            return ActiveReader.GetInt(key);
+        }
+
+        public static bool TryGetFloat(LoogaBlackboardKey key, out float value)
+        {
+            return ActiveReader.TryGetFloat(key, out value);
+        }
+
+        public static float GetFloat(LoogaBlackboardKey key)
+        {
+            return ActiveReader.GetFloat(key);
+        }
+
+        public static bool TryGetString(LoogaBlackboardKey key, out string value)
+        {
+            return ActiveReader.TryGetString(key, out value);
+        }
+
+        public static string GetString(LoogaBlackboardKey key)
+        {
+            return ActiveReader.GetString(key);
+        }
+
+        public static void RemoveValue(LoogaBlackboardKey key)
+        {
+            ActiveWriter?.RemoveValue(key);
+        }
     }
 }
--- Runtime/LoogaBlackboardRuntimeRegistry.cs
+++ Runtime/LoogaBlackboardRuntimeRegistry.cs
@@ -18,5 +18,80 @@ namespace LoogaSoft.Blackboard
         {
             LoogaBlackboardRegistry.ClearActive(blackboard);
         }
+
+        public static void SetValue(LoogaBlackboardKey key, LoogaBlackboardValue value)
+        {
+            LoogaBlackboardRegistry.SetValue(key, value);
+        }
+
+        public static void SetBool(LoogaBlackboardKey key, bool value)
+        {
+            LoogaBlackboardRegistry.SetBool(key, value);
+        }
+
+        public static void SetInt(LoogaBlackboardKey key, int value)
+        {
+            LoogaBlackboardRegistry.SetInt(key, value);
+        }
+
+        public static void SetFloat(LoogaBlackboardKey key, float value)
+        {
+            LoogaBlackboardRegistry.SetFloat(key, value);
+        }
+
+        public static void SetString(LoogaBlackboardKey key, string value)
+        {
+            LoogaBlackboardRegistry.SetString(key, value);
+        }
+
+        public static bool TryGetValue(LoogaBlackboardKey key, out LoogaBlackboardValue value)
+        {
+            return LoogaBlackboardRegistry.TryGetValue(key, out value);
+        }
+
+        public static bool TryGetBool(LoogaBlackboardKey key, out bool value)
+        {
+            return LoogaBlackboardRegistry.TryGetBool(key, out value);
+        }
+
+        public static bool GetBool(LoogaBlackboardKey key)
+        {
+            return LoogaBlackboardRegistry.GetBool(key);
+        }
+
+        public static bool TryGetInt(LoogaBlackboardKey key, out int value)
+        {
+            return LoogaBlackboardRegistry.TryGetInt(key, out value);
+        }
+
+        public static int GetInt(LoogaBlackboardKey key)
+        {
+            return LoogaBlackboardRegistry.GetInt(key);
+        }
+
+        public static bool TryGetFloat(LoogaBlackboardKey key, out float value)
+        {
+            return LoogaBlackboardRegistry.TryGetFloat(key, out value);
+        }
+
+        public static float GetFloat(LoogaBlackboardKey key)
+        {
+            return LoogaBlackboardRegistry.GetFloat(key);
+        }
+
+        public static bool TryGetString(LoogaBlackboardKey key, out string value)
+        {
+            return LoogaBlackboardRegistry.TryGetString(key, out value);
+        }
+
+        public static string GetString(LoogaBlackboardKey key)
+        {
+            return LoogaBlackboardRegistry.GetString(key);
+        }
+
+        public static void RemoveValue(LoogaBlackboardKey key)
+        {
+            LoogaBlackboardRegistry.RemoveValue(key);
+        }
     }
 }
